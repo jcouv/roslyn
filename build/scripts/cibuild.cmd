ea9e3cc5d628d44d30ce8b47f896bbb90e3b51e8
Julien Couvreur <jcouv@microsoft.com>
2018-05-08 18:45:30 -0700

    Update toolset compiler

--- build/scripts/cibuild.cmd
+++ build/scripts/cibuild.cmd
@@ -1,2 +1,3 @@
 @echo off
-powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -bootstrap -binaryLog %*
+powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -binaryLog %*
+REM PROTOTYPE(NullableDogfood) powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build.ps1" -cibuild -build -restore -bootstrap -binaryLog %*
