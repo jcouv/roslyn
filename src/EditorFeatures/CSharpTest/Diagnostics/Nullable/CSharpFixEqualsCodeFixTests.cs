223da1d643d90380478aef91c5e0046b082bcf46
Julien Couvreur <jcouv@microsoft.com>
2018-12-14 22:01:11 -0800

    Add code fix provider

---
+++ src/EditorFeatures/CSharpTest/Diagnostics/Nullable/CSharpFixEqualsCodeFixTests.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.CodeFixes.DeclareAsNullable;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Test.Utilities;
+using Roslyn.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.CodeAnalysis.Editor.CSharp.UnitTests.Diagnostics.DeclareAsNullable
+{
+    [Trait(Traits.Feature, Traits.Features.CodeActionsDeclareAsNullable)]
+    public class CSharpFixEqualsCodeFixTests : AbstractCSharpDiagnosticProviderBasedUserDiagnosticTest
+    {
+        internal override (DiagnosticAnalyzer, CodeFixProvider) CreateDiagnosticProviderAndFixer(Workspace workspace)
+            => (null, new CSharpFixEqualsCodeFixProvider());
+
+        private static readonly TestParameters s_nullableFeature = new TestParameters(parseOptions: new CSharpParseOptions(LanguageVersion.CSharp8));
+
+        [Fact]
+        public async Task FixAll()
+        {
+            await TestInRegularAndScript1Async(
+@"
+class Program
+{
+    static string M()
+    {
+        return {|FixAllInDocument:null|};
+    }
+    static string M2(bool b)
+    {
+        if (b)
+            return null;
+        else
+            return null;
+    }
+}",
+@"
+class Program
+{
+    static string? M()
+    {
+        return null;
+    }
+    static string? M2(bool b)
+    {
+        if (b)
+            return null;
+        else
+            return null;
+    }
+}", parameters: s_nullableFeature);
+        }
+
+        [Fact]
+        public async Task FixNotEquals()
+        {
+            await TestInRegularAndScript1Async(
+@"
+class Program
+{
+    void M(TypeSymbol x)
+    {
+        _ = [|x != x|];
+    }
+}" + typeSymbol,
+@"
+class Program
+{
+    void M(TypeSymbol x)
+    {
+        _ = !TypeSymbol.Equals(x, x, TypeCompareKind.ConsiderEverything2);
+    }
+}" + typeSymbol, parameters: s_nullableFeature);
+        }
+
+        [Fact]
+        public async Task FixEquals()
+        {
+            await TestInRegularAndScript1Async(
+@"
+class Program
+{
+    void M(TypeSymbol x)
+    {
+        _ = [|x == x|];
+    }
+}" + typeSymbol,
+@"
+class Program
+{
+    void M(TypeSymbol x)
+    {
+        _ = TypeSymbol.Equals(x, x, TypeCompareKind.ConsiderEverything2);
+    }
+}" + typeSymbol, parameters: s_nullableFeature);
+        }
+
+        const string typeSymbol = @"
+public class TypeSymbol
+{
+    [System.Obsolete(""obsolete"", error: true)]
+    public static bool operator ==(TypeSymbol left, TypeSymbol right)
+    {
+        throw null;
+    }
+
+    [System.Obsolete(""obsolete"", error: true)]
+    public static bool operator !=(TypeSymbol left, TypeSymbol right)
+    {
+        throw null;
+    }
+}";
+    }
+}
---
+++ src/Features/CSharp/Portable/CodeFixes/Nullable/CSharpFixEqualsCodeFixProvider.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Shared.Extensions;
+using Microsoft.CodeAnalysis.Simplification;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.CSharp.CodeFixes.DeclareAsNullable
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = PredefinedCodeFixProviderNames.DeclareAsNullable), Shared]
+    internal class CSharpFixEqualsCodeFixProvider : SyntaxEditorBasedCodeFixProvider
+    {
+        // error CS0619
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create("CS0619");
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var node = root.FindNode(diagnostic.Location.SourceSpan, getInnermostNodeForTie: true);
+
+            //var declarationTypeToFix = TryGetDeclarationTypeToFix(node);
+            //if (declarationTypeToFix == null)
+            //{
+            //    return;
+            //}
+
+            context.RegisterCodeFix(new MyCodeAction(
+                c => FixAsync(context.Document, diagnostic, c)),
+                context.Diagnostics);
+        }
+
+        protected override Task FixAllAsync(
+            Document document, ImmutableArray<Diagnostic> diagnostics,
+            SyntaxEditor editor, CancellationToken cancellationToken)
+        {
+            var root = editor.OriginalRoot;
+
+            foreach (var diagnostic in diagnostics)
+            {
+                var node = diagnostic.Location.FindNode(getInnermostNodeForTie: true, cancellationToken);
+                FixEquals(document, editor, node);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static void FixEquals(Document document, SyntaxEditor editor, SyntaxNode node)
+        {
+            var comparisonToFix = TryGetComparisonToFix(node);
+            if (comparisonToFix != null)
+            {
+                ExpressionSyntax fixedComparison =
+                    SyntaxFactory.InvocationExpression(
+                        SyntaxFactory.QualifiedName(SyntaxFactory.IdentifierName("TypeSymbol"), SyntaxFactory.IdentifierName("Equals")),
+                        SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new[] {
+                            SyntaxFactory.Argument(comparisonToFix.Left),
+                            SyntaxFactory.Argument(comparisonToFix.Right),
+                            SyntaxFactory.Argument(SyntaxFactory.QualifiedName(SyntaxFactory.IdentifierName("TypeCompareKind"), SyntaxFactory.IdentifierName("ConsiderEverything2")))
+                        })));
+
+                if (comparisonToFix.OperatorToken.IsKind(SyntaxKind.ExclamationEqualsToken))
+                {
+                    fixedComparison = SyntaxFactory.PrefixUnaryExpression(SyntaxKind.LogicalNotExpression, fixedComparison);
+                }
+                fixedComparison = fixedComparison.WithAdditionalAnnotations(Simplifier.Annotation);
+                editor.ReplaceNode(comparisonToFix, fixedComparison);
+            }
+        }
+
+        private static BinaryExpressionSyntax TryGetComparisonToFix(SyntaxNode node)
+        {
+            if (!node.IsKind(SyntaxKind.EqualsExpression, SyntaxKind.NotEqualsExpression))
+            {
+                return null;
+            }
+            return (BinaryExpressionSyntax)node;
+        }
+
+        private class MyCodeAction : CodeAction.DocumentChangeAction
+        {
+            public MyCodeAction(Func<CancellationToken, Task<Document>> createChangedDocument) :
+                base(CSharpFeaturesResources.Declare_as_nullable,
+                     createChangedDocument,
+                     CSharpFeaturesResources.Declare_as_nullable)
+            {
+            }
+        }
+    }
+}
